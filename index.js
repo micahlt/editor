f8dcd5e3dae6495bb7485c9684f286abe484eb92
B294 <64226546+B294@users.noreply.github.com>
2020-05-27 11:28:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ resize();
 
 // resize canvas when window is resized
 function resize() {
-  ctx.canvas.width = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
+  ctx.canvas.width = window.20;
+  ctx.canvas.height = window.20;
 }
