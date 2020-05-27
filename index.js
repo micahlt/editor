85800a1a29623bef0c4e75fcb8f8d01db3f966fc
B294 <64226546+B294@users.noreply.github.com>
2020-05-27 11:18:59 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var canvas = document.getElementById("draw");
+
+var ctx = canvas.getContext("2d");
+resize();
+
+// resize canvas when window is resized
+function resize() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+}
